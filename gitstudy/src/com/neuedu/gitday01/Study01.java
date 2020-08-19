c4118650ca67180f94861f89a0385753969633a6
13066593539 <675473910@qq.com>
2020-08-19 16:21:53 +0800

    hello

--- gitstudy/src/com/neuedu/gitday01/Study01.java
+++ gitstudy/src/com/neuedu/gitday01/Study01.java
@@ -2,8 +2,7 @@ package com.neuedu.gitday01;
 
 public class Study01 {
 	public static void main(String[] args) {
-		System.out.println("1");
-		System.out.println("21");
+		System.out.println("hello world");
 		System.out.println("3");
 		System.out.println("4");
 	}
