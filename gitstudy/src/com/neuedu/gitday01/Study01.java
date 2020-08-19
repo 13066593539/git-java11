7d5061bb81e87b33bf072c99ec8a9087c0026a73
13066593539 <675473910@qq.com>
2020-08-19 16:17:22 +0800

    0000

--- gitstudy/src/com/neuedu/gitday01/Study01.java
+++ gitstudy/src/com/neuedu/gitday01/Study01.java
@@ -1,10 +1,10 @@
 package com.neuedu.gitday01;
 
 public class Study01 {
-public static void main(String[] args) {
-	System.out.println("001");
-	System.out.println("001");
-	System.out.println("001");
-	System.out.println("001");
-}
+	public static void main(String[] args) {
+		System.out.println("001");
+		System.out.println("001");
+		System.out.println("001");
+		System.out.println("001");
+	}
 }
